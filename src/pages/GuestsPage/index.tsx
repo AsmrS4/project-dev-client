dd61c6a19cf83dabd21dd02ed6c17ca96fea65d6
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-06-30 01:04:26 +0700

    added guests page

---
+++ src/pages/GuestsPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GuestPage.scss';
+const GuestsPage = () => {
+    return (
+        <section className='guestsPage'>
+            <div className='pageHeader'>
+                <h1>Список гостей</h1>
+            </div>
+        </section>
+    );
+};
+
+export default GuestsPage;
