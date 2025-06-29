fa56627d4aaa4389a2be856cef68b3b5ac957c62
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-06-29 21:33:59 +0700

    save commit

--- src/pages/ArchivedEvent/index.tsx
+++ src/pages/ArchivedEvent/index.tsx
@@ -9,11 +9,21 @@ import { DateConverter } from '@utils/converter/DateConverter';
 import { useDispatch } from 'react-redux';
 import { clearSession } from '@store/User/AuthReducer';
 
+interface ReviewProps {
+    reviews: any[];
+    totalRating: number;
+}
+
 const ArhivedEventPage = () => {
     const { id } = useParams();
     const dateConverter: DateConverter = new DateConverter();
     const { token, role } = useAppSelector((state) => state.authReducer);
+    const [ratingClass, setClass] = useState<string>('');
 
+    const [reviews, setReviews] = useState<ReviewProps>({
+        reviews: [],
+        totalRating: 0.0,
+    });
     const [eventDetails, setDetails] = useState<IEvent>({
         id: '',
         images: [],
@@ -27,7 +37,37 @@ const ArhivedEventPage = () => {
 
     const dispatch: any = useDispatch();
     const navigate: any = useNavigate();
+    const fetchReviews = async () => {
+        try {
+            const response = await axios({
+                url: `http://localhost:8090/api/history/event/${id}/reviews`,
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log(response.data);
+            setReviews(response.data);
+        } catch (error) {
+            if (error instanceof AxiosError && error.response) {
+                switch (error.response.status) {
+                    case 401: {
+                        dispatch(clearSession());
+                        navigate('/auth/sign-in');
+                        break;
+                    }
+                    case 403: {
+                        navigate('/error/forbidden');
+                        break;
+                    }
 
+                    default: {
+                        navigate('/error/server');
+                    }
+                }
+            }
+        }
+    };
     const fetchDetails = async () => {
         try {
             const response = await axios({
@@ -62,14 +102,34 @@ const ArhivedEventPage = () => {
     };
     useEffect(() => {
         fetchDetails();
+        fetchReviews();
         console.log(eventDetails);
     }, []);
+    useEffect(() => {
+        if (reviews.totalRating == 0) {
+            return setClass('default');
+        }
+        if (reviews.totalRating > 0 && reviews.totalRating < 3.0) {
+            return setClass('bad');
+        }
+        if (reviews.totalRating >= 3.0 && reviews.totalRating <= 4.2) {
+            return setClass('good');
+        }
+        if (reviews.totalRating > 4.2) {
+            return setClass('great');
+        }
+    }, [reviews]);
     return (
         <section className={styles.eventPage}>
             <div className={styles.eventHeader}>
                 <h1>{eventDetails.title}</h1>
             </div>
-            <ImageCarousel images={eventDetails.images}></ImageCarousel>
+            <div className={styles.carouselWrapper}>
+                <div className={styles.rating}>
+                    <span className={ratingClass}>{reviews.totalRating}</span>
+                </div>
+                <ImageCarousel images={eventDetails.images}></ImageCarousel>
+            </div>
             <div className={styles.eventContainer}>
                 <div className={styles.details}>
                     <>
